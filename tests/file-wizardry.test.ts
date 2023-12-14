35ac4684ee4005596daa507ca1fb34b9ac5ee1e5
devFresher <fresher.dev01@gmail.com>
2023-12-14 22:31:00 +0100

    Refactor tests

--- tests/file-wizardry.test.ts
+++ tests/file-wizardry.test.ts
@@ -8,190 +8,160 @@ import { FileWizardry, UploadOptions, storageTypes } from '../src/';
 jest.mock('aws-sdk');
 jest.mock('multer-s3');
 
-let app = express();
+let app: express.Application;
 
 const fileWizardry = new FileWizardry('memory');
 
+beforeEach(() => {
+  jest.resetModules();
+  app = express();
+});
+
 describe('FileWizardry', () => {
-  describe('uploadFile', () => {
-    beforeEach(() => {
-      jest.resetModules();
-      app = express();
+  describe('upload', () => {
+    const executeUpload = (options: UploadOptions) => {
+      app.post('/upload', fileWizardry.upload(options), (req, res) => {
+        const requestWithValidation = req as express.Request & { fileValidationError?: Error };
+
+        if (requestWithValidation.fileValidationError) {
+          console.log(requestWithValidation.fileValidationError);
+
+          return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
+        }
+
+        res.json({ message: 'File uploaded successfully' });
+      });
+    };
+
+    it('should handle file uploads successfully', (done) => {
+      const uploadOptions: UploadOptions = {
+        formats: ['image/jpeg', 'image/png'],
+        fieldName: 'image',
+      };
+
+      executeUpload(uploadOptions);
+
+      request(app)
+        .post('/upload')
+        .attach('image', './tests/data/img1.png')
+        .expect(200, { message: 'File uploaded successfully' }, done);
     });
 
-    describe('Successful File Upload', () => {
-      it('should handle file uploads successfully', (done) => {
-        const uploadOptions: UploadOptions = {
-          formats: ['image/jpeg', 'image/png'],
-          fieldName: 'image',
-        };
+    it('should handle invalid file format', (done) => {
+      const uploadOptions: UploadOptions = {
+        formats: ['image/jpeg', 'image/png'],
+        fieldName: 'doc',
+      };
 
-        app.post('/upload', fileWizardry.uploadFile(uploadOptions), (req, res) => {
-          const requestWithValidation = req as express.Request & { fileValidationError?: Error };
-          if (requestWithValidation.fileValidationError) {
-            return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
-          }
-          res.json({ message: 'File uploaded successfully' });
+      executeUpload(uploadOptions);
+
+      request(app)
+        .post('/upload')
+        .attach('doc', './tests/data/rptstdholder.pdf')
+        .expect(400)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body.error).toContain('Invalid file format');
+          done();
         });
-
-        request(app)
-          .post('/upload')
-          .attach('image', './tests/data/img1.png')
-          .expect(200)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.body.message).toEqual('File uploaded successfully');
-            done();
-          });
-      });
     });
 
-    describe('Invalid File Format', () => {
-      it('should handle invalid file format', (done) => {
-        const uploadOptions: UploadOptions = {
-          formats: ['image/jpeg', 'image/png'],
-          fieldName: 'doc',
-        };
+    it('should handle file size exceeding limit', (done) => {
+      const uploadOptions: UploadOptions = {
+        formats: ['image/jpeg', 'image/png'],
+        fieldName: 'image',
+        maxSize: 1,
+      };
 
-        app.post('/upload', fileWizardry.uploadFile(uploadOptions), (req, res) => {
-          const requestWithValidation = req as express.Request & { fileValidationError?: Error };
-          if (requestWithValidation.fileValidationError) {
-            return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
-          }
-          res.json({ message: 'File uploaded successfully' });
+      executeUpload(uploadOptions);
+
+      request(app)
+        .post('/upload')
+        .attach('image', './tests/data/img1.png') // a file larger than the allowed limit
+        .expect(400)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.body.error).toMatch(/File too large|File size exceeds the allowed limit/);
+          done();
         });
-
-        request(app)
-          .post('/upload')
-          .attach('doc', './tests/data/rptstdholder.pdf')
-          .expect(400)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.body.error).toContain('Invalid file format');
-            done();
-          });
-      });
     });
 
-    describe('File Size Exceeding Limit', () => {
-      it('should handle file size exceeding limit', (done) => {
-        const uploadOptions: UploadOptions = {
-          formats: ['image/jpeg', 'image/png'],
-          fieldName: 'image',
-          maxSize: 1,
-        };
+    it('should handle no file upload error', (done) => {
+      const uploadOptions: UploadOptions = {
+        formats: ['image/jpeg', 'image/png'],
+        fieldName: 'image',
+        maxSize: 1,
+      };
 
-        app.post('/upload', fileWizardry.uploadFile(uploadOptions), (req, res) => {
-          const requestWithValidation = req as express.Request & { fileValidationError?: Error };
-
-          if (requestWithValidation.fileValidationError) {
-            return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
-          }
-          res.json({ message: 'File uploaded successfully' });
-        });
-
-        request(app)
-          .post('/upload')
-          .attach('image', './tests/data/img1.png') // a file larger than the allowed limit
-          .expect(400)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.body.error).toContain('File size exceeds the allowed limit');
-            done();
-          });
-      });
+      executeUpload(uploadOptions);
+      request(app).post('/upload').attach('image', '').expect(400, { error: 'No file uploaded.' }, done);
     });
 
-    describe('No File Uploaded', () => {
-      it('should handle no file upload error', (done) => {
-        const uploadOptions: UploadOptions = {
-          formats: ['image/jpeg', 'image/png'],
-          fieldName: 'image',
-          maxSize: 1,
-        };
+    it('should handle invalid field name', (done) => {
+      const uploadOptions: UploadOptions = {
+        formats: ['image/jpeg', 'image/png'],
+        fieldName: 'image',
+      };
 
-        app.post('/upload', fileWizardry.uploadFile(uploadOptions), (req, res) => {
-          const requestWithValidation = req as express.Request & { fileValidationError?: Error };
+      executeUpload(uploadOptions);
 
-          if (requestWithValidation.fileValidationError) {
-            return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
-          }
-          res.json({ message: 'File uploaded successfully' });
-        });
-
-        request(app)
-          .post('/upload')
-          .attach('image', '')
-          .expect(400)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.body.error).toContain('No file');
-            done();
-          });
-      });
+      request(app)
+        .post('/upload')
+        .attach('images', './tests/data/img1.png')
+        .expect(400, { error: 'Unexpected field' }, done);
     });
 
-    describe('Invalid Field Name', () => {
-      it('should handle invalid field name', (done) => {
-        const uploadOptions: UploadOptions = {
-          formats: ['image/jpeg', 'image/png'],
-          fieldName: 'image',
-        };
+    it('handles multiple file uploads successfully', (done) => {
+      const uploadOptions: UploadOptions = {
+        fieldName: 'files',
+        formats: ['image/jpeg', 'image/png'],
+        multiFile: true,
+      };
 
-        app.post('/upload', fileWizardry.uploadFile(uploadOptions), (req, res) => {
-          const requestWithValidation = req as express.Request & { fileValidationError?: Error };
+      executeUpload(uploadOptions);
 
-          if (requestWithValidation.fileValidationError) {
-            return res.status(400).json({ error: requestWithValidation.fileValidationError.message });
-          }
-          res.json({ message: 'File uploaded successfully' });
-        });
-
-        request(app)
-          .post('/upload')
-          .attach('images', './tests/data/img1.png')
-          .expect(400)
-          .end((err, res) => {
-            if (err) return done(err);
-            expect(res.body.error).toContain('Unexpected field');
-            done();
-          });
-      });
+      request(app)
+        .post('/upload')
+        .attach('files', './tests/data/img1.png')
+        .attach('files', './tests/data/img1.png')
+        .expect(200, { message: 'File uploaded successfully' }, done);
     });
   });
 
   describe('setStorageType', () => {
-    describe('positive cases', () => {
-      it('should change the storageType to memory', () => {
-        fileWizardry.setStorageType('memory');
-        expect(fileWizardry.getStorage()).toBeInstanceOf(multer.memoryStorage().constructor);
-      });
-      it('should change the storageType to disk', () => {
-        fileWizardry.setStorageType('disk', { destination: './tests/data/uploads/folder' });
-        expect(fileWizardry.getStorage()).toBeInstanceOf(multer.diskStorage({}).constructor);
-      });
-      it('should change the storageType to amazonS3', () => {
-        const s3Options = { bucket: 'my', region: 'NG' };
-        fileWizardry.setStorageType('amazons3', s3Options);
-        expect(multerS3).toHaveBeenCalledWith({
-          s3: expect.anything(),
-          bucket: s3Options.bucket,
-          acl: 'public-read',
-          contentType: multerS3.AUTO_CONTENT_TYPE,
-          key: expect.any(Function),
-        });
+    it('changes the storageType to memory', () => {
+      fileWizardry.setStorageType('memory');
+      expect(fileWizardry.getStorage()).toBeInstanceOf(multer.memoryStorage().constructor);
+    });
+
+    it('changes the storageType to disk', () => {
+      const destination = './tests/data/uploads/folder' as storageTypes.DiskStorageOptions['destination'];
+      fileWizardry.setStorageType('disk', { destination });
+      expect(fileWizardry.getStorage()).toBeInstanceOf(multer.diskStorage({}).constructor);
+    });
+
+    it('changes the storageType to amazonS3', () => {
+      const s3Options = { bucket: 'my', region: 'NG' } as storageTypes.S3StorageTypeOptions;
+      fileWizardry.setStorageType('amazons3', s3Options);
+      expect(multerS3).toHaveBeenCalledWith({
+        s3: expect.anything(),
+        bucket: s3Options.bucket,
+        acl: 'public-read',
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        key: expect.any(Function),
       });
     });
 
-    describe('negative cases', () => {
-      it('should return an error if s3 storage option is not provided', () => {
-        const invalidCall = () => fileWizardry.setStorageType('amazons3');
-        expect(invalidCall).toThrow('S3 storage options are required. Provide options for S3 storage.');
-      });
-      it('should return an error if invalid/unsupported storage type is provided', () => {
-        const invalidCall = () => fileWizardry.setStorageType('aaa' as unknown as storageTypes.StorageType);
-        expect(invalidCall).toThrow('Invalid storage type.');
-      });
+    it('throws an error if s3 storage option is not provided', () => {
+      expect(() => fileWizardry.setStorageType('amazons3')).toThrow(
+        'S3 storage options are required. Provide options for S3 storage.',
+      );
+    });
+
+    it('throws an error if an invalid/unsupported storage type is provided', () => {
+      expect(() => fileWizardry.setStorageType('aaa' as unknown as storageTypes.StorageType)).toThrow(
+        'Invalid storage type.',
+      );
     });
   });
 });
